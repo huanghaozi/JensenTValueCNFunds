5f7b6450c9d18ce7c4af0429dc824a1f153f2a81
HH <huang_hao_zi@163.com>
2020-11-19 15:33:08 +0800

    Finished all

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# huanghaozi-JensenTValueCNFunds
+# 使用说明
+
+##安装Python或Conda
+
+并在安装时将其加入PATH环境变量，MiniConda清华源下载地址：
+	
+`https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-py38_4.8.3-Windows-x86_64.exe`
+	
+## 换源
+	
+运行命令：
+
+```shell
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+```
+
+## 解压
+	
+完整解压此zip，记录解压路径
+	
+## 安装依赖环境
+	
+运行命令：
+	
+```shell
+cd /d 路径		（将"路径"换为解压路径）
+pip install -r requirements.txt
+```
+	
+## 自定义代码
+	
+用记事本打开`fund_crawl_capm.py`
+	
+其中的`code_num`表示随机选取的基金数量（默认10只）
+	
+`index_code`表示指数代码（默认沪深300）
+	
+可自行修改
+	
+## 运行代码
+
+```shell
+python fund_crawl_capm.py
+```
+	
+### 程序将输出所爬取的无风险利率、基金代码及其与指数回归所得的alpha的t值
+### 程序会自动将原始数据输出至原始数据文件夹中
---
+++ fund_crawl_Jensen.py
@@ -0,0 +1,42 @@
+import statsmodels.api as sm
+import akshare as ak
+import pandas as pd
+import numpy as np
+import random
+
+code_num = 10
+index_code = 'sh000300'
+
+codes = pd.read_excel('fund_codes.xlsx', index_col=0, dtype={'基金代码': 'object'})['基金代码']
+
+# 获取指数组合index_code的收益率数据
+df_M = ak.stock_zh_index_daily_em(symbol=index_code)
+df_M['updown'] = 0.0
+for index, row in df_M.iterrows():
+    if index > 0:
+        df_M['updown'][index] = (float(row['close']) - float(df_M['close'][index-1])) / float(df_M['close'][index-1])
+df_M = df_M[['date', 'updown']]
+
+# 获取最新Shibor作为无风险收益率
+df_rf = ak.rate_interbank(market="上海银行同业拆借市场", symbol="Shibor人民币", indicator="3月", need_page="1")
+print('无风险利率:' + str(df_rf['利率(%)'][0]) + '%\n')
+rf = np.power(1+df_rf['利率(%)'][0]/100, 1/360)-1  # 转为日利率
+
+# 随机抽取code_num只基金获取收益率数据并回归
+codes = random.sample(list(codes), code_num)
+print('基金代码' + '\t' + 'Alpha的T值')
+for code in codes:
+    df_i = ak.fund_em_open_fund_info(fund=code, indicator="单位净值走势").rename(columns={'x': 'date'})
+    for index, row in df_i.iterrows():
+        df_i['date'][index] = str(df_i['date'][index])
+        if index > 0:
+            df_i['equityReturn'][index] = (float(row['y']) - float(df_i['y'][index-1])) / float(df_i['y'][index-1])
+    df_i = df_i[['date', 'equityReturn']]
+    df = pd.merge(df_M, df_i, on='date')
+    y = df['equityReturn'].values - rf
+    x = sm.add_constant(df['updown'].values - rf)
+    df['Ri'] = y
+    df['Rm'] = df['updown'].values - rf
+    df.to_excel('./原始数据/' + code + '.xlsx')
+    model = sm.OLS(endog=y, exog=x).fit()
+    print(code + '\t' + str(model.tvalues[0]))
